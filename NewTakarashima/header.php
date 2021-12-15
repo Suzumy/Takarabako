7b349dae1d8d04e9098d6608584e5483b22f634b
RikutoOchi <gurunya121@gmail.com>
2021-12-15 13:27:22 +0900

    メイン画面作成
    メイン画面を作成中
    ヘッダと基礎部分は出来ています

---
+++ NewTakarashima/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<style>
+    body {
+        background: #ffb899;
+    }
+
+    #title {
+        color: white;
+        text-align: center;
+    }
+
+    #image {
+        position: fixed;
+        width: 50px;
+        height: 50px;
+        z-index: 10;
+    }
+
+    hr {
+        color: black;
+    }
+</style>
+
+<head>
+    <meta charset="UTF=8">
+    <title>たからばこ</title>
+    <!-- <link rel="stylesheet" href="/NewTakarashima/style/homeStyle.css"> -->
+</head>
+
+<body>
+    <img id="image" src="image/takarabako1.png">
+    <div id="title">
+        <h1>たからばこ</h1>
+    </div>
+    <hr color="black">
+</body>
