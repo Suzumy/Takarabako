1fe186a005c3b37cdd74030ca6e633b1bf1815d2
ryosuke125 <85324138+ryosuke125@users.noreply.github.com>
2021-12-15 13:37:48 +0900

    iframeの調整

---
+++ 3_i/deadline/list.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>趣味一覧画面</title>
+</head>
+<?php
+//htmlspecialcharsの処理
+require_once 'util.php';
+
+//ホップアップの表示
+require_once 'hopup.php';
+
+//締め切り要素をlistDB.phpから取得
+require_once 'listDB.php';
+
+?>
+<!-- ここからサイトの表示-->
+<h1>締め切り一覧 </h1>
+<main>
+
+    <form method='POST'>
+        <select name='tag'>
+            <option value='全て'>全て</option>
+            <?php
+            //tagを取得 
+            foreach ($tags as $tags) {
+                $tags_list = "<option value='" . h($tags['tag']);
+                $tags_list .= "'>" . h($tags['tag']) . "</option>";
+                echo $tags_list;
+            }
+            ?>
+        </select>
+        <input type='submit' value='送信' />
+    </form>
+    <!-- ここから締め切りリスト-->
+    <table border="1">
+        <tr>
+            <th>タイトル</th>
+            <th>締切日</th>
+            <th>詳細</th>
+        </tr>
+
+        <?php
+        foreach ($tasks as $task) {
+        ?>
+            <tr>
+                <td><?php echo h($task['title']); ?></td>
+                <td><?php echo h($task['deadline']); ?></td>
+                <td><?php echo h($task['detail']); ?></td>
+                <td>
+                    <!-- 編集画面edit.phpにデータを送信-->
+                    <!-- edit.phpをregister_Deadlineと共有にしたい -->
+                    <form action="edit.php" method="POST">
+                        <input type="hidden" name="id" value="<?= $task['id']; ?>">
+                        <input type="hidden" name="title" value="<?= $task['title']; ?>">
+                        <input type="hidden" name="detail" value="<?= $task['detail']; ?>">
+                        <input type="hidden" name="deadline" value="<?= $task['deadline']; ?>">
+                        <input type="submit" name="btn" value="編集">
+                    </form>
+                    <!-- 削除画面delete.phpにデータを送信-->
+                    <form action="delete.php" method="POST">
+                        <input type="hidden" name="title" value="<?= $task['title']; ?>">
+                        <input type="hidden" name="detail" value="<?= $task['detail']; ?>">
+                        <input type="hidden" name="deadline" value="<?= $task['deadline']; ?>">
+                        <input type="hidden" name="id" value="<?= $task['id']; ?>">
+                        <input type="submit" name="btn" value="削除">
+                    </form>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+
+    </table>
+    <a href="register_Deadline.php">新規登録</a>
+</main>
