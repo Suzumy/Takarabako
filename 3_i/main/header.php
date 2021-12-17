dbf61f73be1fd32b58e27c71cf97a8357f969af0
ryosuke125 <85324138+ryosuke125@users.noreply.github.com>
2021-12-17 11:20:19 +0900

    main追加
    マイン画面のinnerにのリストにそれぞれの画面遷移を追加

---
+++ 3_i/main/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>サンプル</title>
+    <link rel="stylesheet" href="HP.css">
+</head>
+
+<body>
+    <div id=navArea>
+        <nav>
+            <div class="inner">
+                <ul>
+                    <li><a href="https://localhost/3_i/3_i/main/HP.php">home</a></li>
+                    <li><a href="https://localhost/3_i/3_i/hobby/register_Hobby.php">趣味・画像登録</a></li>
+                    <li><a href="#">趣味・画像一覧</a></li>
+                    <li><a href="https://localhost/3_i/3_i/deadline/register_Deadline.php">締め切り登録</a></li>
+                    <li><a href="https://localhost/3_i/3_i/deadline/list.php">締め切り一覧</a></li>
+                    <li><a href="#">設定</a></li>
+                    
+                </ul>
+            </div>
+        </nav>
+
+        <header>
+            <p><a href="#"><img src="box.jpg" alt="宝箱" class="image1"></a></p>
+            <h1>たからばこ</h1>
+            <a href="new.html" class="btn">ログイン</a>
+            <a href="rogin.html" class="btn2">新規登録</a>
+        </header>
+
+    <div class="toggle-btn">
+    <!--ハンバーガーメニュー-->
+    <span></span>
+    <span></span>
+    <span></span>
+    </div>
+
+<div id="mask"></div>
+
