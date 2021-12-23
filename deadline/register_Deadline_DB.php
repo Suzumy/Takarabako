cbbf8a666ac7cdb24df92ca051679c52067f66fe
ryosuke125 <85324138+ryosuke125@users.noreply.github.com>
2021-12-24 00:06:26 +0900

    登録画面編集
    登録画面にtagを追加既存のタグであれば登録可能

--- deadline/register_Deadline_DB.php
+++ deadline/register_Deadline_DB.php
@@ -4,6 +4,7 @@ $title = $_POST['title'];
 $days = $_POST['day'];
 $time = $_POST['time'];
 $detail = $_POST['memo'];
+$tag = $_POST['tag'];
 $data = $days . ' ' . $time;
 
 $sql = "INSERT INTO `application_lists` (`id`,`title`, `deadline`, `detail`) VALUES (NULL,:title, :data, :detail )";
@@ -17,11 +18,20 @@ $sth->bindValue(':data', $data);
 $sth->bindValue(':detail', $detail);
 $result1 = $sth->execute();
 #格納したidを取得lasrinsertid()
-$apl_id = $pdo -> lastInsertId();
+$apl_id = $pdo->lastInsertId();
+//tag_idの取得
+$sql = 'SELECT * FROM tags WHERE tag=:tag';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':tag', $tag);
+$stmt->execute();
+$tags_id = $stmt->fetch();
+$tags_id = $tags_id['id'];
 
-$sql = "INSERT INTO `apl_tag` (`id`,`apl_id`, `tag_id`) VALUES (NULL, :apl_id,'1')";
+$sql = "INSERT INTO `apl_tag` (`id`,`apl_id`, `tag_id`) VALUES (NULL, :apl_id,:apl_tag)";
 $sth = $pdo->prepare($sql);
+
 $sth->bindValue(':apl_id', $apl_id);
+$sth->bindValue(':apl_tag', $tags_id);
 $result2 = $sth->execute();
 
 
