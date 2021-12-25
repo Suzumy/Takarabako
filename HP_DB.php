914f7f5ebdd3ed385d2e6ba1c0ed2e5f99329ac5
ryosuke125 <85324138+ryosuke125@users.noreply.github.com>
2021-12-26 01:37:44 +0900

    Update HP_DB.php
    tag統合

--- HP_DB.php
+++ HP_DB.php
@@ -1,11 +1,14 @@
 <?php
 //DBへのアクセス
 require_once __DIR__ . '/DB.php';
-
-$pdo = new PDO($dsn, $user, $password);
-$sql = "SELECT * FROM hobbys";
+$sql = "SELECT  * FROM tags";
 $stmt = $pdo->query($sql);
+$stmt->execute();
+$tags = $stmt->fetchall();
+
+new PDO($dsn, $user, $password);
+$sql = "SELECT * FROM hobbys";
+$stmt = $pdo->prepare($sql);
 $stmt->execute();
 $all = $stmt->fetchall();
 $num = 0;
-?>
