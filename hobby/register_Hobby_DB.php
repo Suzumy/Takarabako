e6c9a186047faa0642872eebe67b1e9b15070910
Suzumy <ryouga.0085@gmail.com>
2021-12-31 01:52:54 +0900

    趣味登録においてユーザーIDも登録するように変更
    userIdの欄に仮置きしていた数値をsessionから取得してくるように変更

--- hobby/register_Hobby_DB.php
+++ hobby/register_Hobby_DB.php
@@ -1,17 +1,19 @@
 <?php
+session_start();
 require_once __DIR__ . '/DB.php';
 $URL = $_POST['URL'];
 $tag = $_POST['tag'];
 $memo = $_POST['memo'];
-$userId = 1; //ここにはセッションに入っているユーザーIDを入れる
+$userId = $_SESSION['id']; //ここにはセッションに入っているユーザーIDを入れる
 
 $data = date("Y/m/d H:i:s");
 $data2 = date("Y/m/d H:i:s");
 
 #hobbysへの登録処理
-$sql = "INSERT INTO `hobbys` (`id`, `user_id`, `URL`, `memo`, `picture`, `day_at`, `disclosure`, `created_day`, `update_day`) VALUES (NULL, '1', :URL, :memo, NULL, NULL, NULL, :data, :data2 )";
+$sql = "INSERT INTO `hobbys` (`id`, `user_id`, `URL`, `memo`, `picture`, `day_at`, `disclosure`, `created_day`, `update_day`) VALUES (NULL, :user_id, :URL, :memo, NULL, NULL, NULL, :data, :data2 )";
 //sqlインクジェクション対策
 $sth = $pdo->prepare($sql);
+$sth->bindValue(':user_id', $userId);
 $sth->bindValue(':URL', $URL);
 $sth->bindValue(':memo', $memo);
 $sth->bindValue(':data', $data);
