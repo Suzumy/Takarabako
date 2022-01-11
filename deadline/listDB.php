a0b513de7cd3ccdbbd88047f75b5575d793fcc83
ryosuke125 <85324138+ryosuke125@users.noreply.github.com>
2022-01-11 23:24:45 +0900

    締め切りの一覧修正および登録の修正
    締め切りの一覧修正および登録の修正

--- deadline/listDB.php
+++ deadline/listDB.php
@@ -1,23 +1,59 @@
 <?php
 //DB接続設定
+<<<<<<< HEAD
+
+session_start();
+
+require_once 'DB.php';
+
+try {
+    $userId = $_SESSION['id'];
+} catch (Exception $e) {
+    //何かの理由でセッションが切れてる場合、ログイン画面へ遷移する
+    header('Location: ../user/login.php');
+}
+=======
 require_once '../DB.php';
 
 session_start();
 
 $userId = $_SESSION['id'];
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> b6dd4a010bc0ffa3c72f109d0a05f65c6f6755ec
+>>>>>>> Stashed changes
 //tagがまず送られている？
 if (isset($_POST['tag'])) {
-    $tag = $_POST['tag'];
+    $apl_tag = $_POST['tag'];
     //プルダウンの選択が全てなら全部表示
+<<<<<<< HEAD
+    if (  $apl_tag  == '全て') {
+        $sql = "SELECT tags.tag,application_lists.detail,application_lists.title,application_lists.deadline,application_lists.id,application_lists.user_id FROM `application_lists` 
+        INNER JOIN apl_tag ON application_lists.id = apl_tag.apl_id 
+        INNER JOIN tags ON apl_tag.tag_id = tags.id WHERE application_lists.user_id = :user_id";
+=======
     if ($tag == '全て') {
         $sql = "SELECT  * FROM application_lists WHERE user_id = :user_id";
+<<<<<<< Updated upstream
+=======
+>>>>>>> b6dd4a010bc0ffa3c72f109d0a05f65c6f6755ec
+>>>>>>> Stashed changes
         $stmt =  $pdo->prepare($sql);
         $stmt->bindValue(':user_id', $userId);
         $stmt->execute();
         $tasks = $stmt->fetchall();
         //プルダウンの文字列に該当するものだけ送信
     } else {
+<<<<<<< HEAD
+        //プレースホルダにするべきかも
+        //tagを確認
+        $sql = "SELECT tags.tag,application_lists.detail,application_lists.title,application_lists.deadline,application_lists.id,application_lists.user_id FROM `application_lists` 
+        INNER JOIN apl_tag ON application_lists.id = apl_tag.apl_id 
+        INNER JOIN tags ON apl_tag.tag_id = tags.id WHERE application_lists.user_id = :user_id AND tags.tag=:tags_tag";
+        $stmt =  $pdo->prepare($sql);
+        $stmt->bindValue(':tags_tag', $apl_tag);
+=======
 
         //tagsテーブルのidを取得
         $sql = 'SELECT * FROM tags WHERE tag=:tag';
@@ -29,13 +65,27 @@ if (isset($_POST['tag'])) {
         $sql = 'SELECT * FROM  application_lists JOIN apl_tag ON application_lists.id=apl_tag.apl_id WHERE apl_tag.tag_id=:tags_id AND user_id = :user_id';
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':tags_id', $tags_id);
+<<<<<<< Updated upstream
+=======
+>>>>>>> b6dd4a010bc0ffa3c72f109d0a05f65c6f6755ec
+>>>>>>> Stashed changes
         $stmt->bindValue(':user_id', $userId);
         $stmt->execute();
         $tasks = $stmt->fetchall();
     }
     //$tagに変数がない場合
 } else {
+<<<<<<< Updated upstream
     $sql = "SELECT  * FROM application_lists WHERE user_id = :user_id";
+=======
+<<<<<<< HEAD
+    $sql = "SELECT tags.tag,application_lists.detail,application_lists.title,application_lists.deadline,application_lists.id,application_lists.user_id FROM `application_lists` 
+    INNER JOIN apl_tag ON application_lists.id = apl_tag.apl_id 
+    INNER JOIN tags ON apl_tag.tag_id = tags.id WHERE application_lists.user_id = :user_id";
+=======
+    $sql = "SELECT  * FROM application_lists WHERE user_id = :user_id";
+>>>>>>> b6dd4a010bc0ffa3c72f109d0a05f65c6f6755ec
+>>>>>>> Stashed changes
     $stmt =  $pdo->prepare($sql);
     $stmt->bindValue(':user_id', $userId);
     $stmt->execute();
--- deadline/register_Deadline_DB.php
+++ deadline/register_Deadline_DB.php
@@ -27,31 +27,69 @@ $result1 = $sth->execute();
 #格納したidを取得lasrinsertid()
 $apl_id = $pdo->lastInsertId();
 
+
+#tagsへの登録処理
+#複数のタグをスペースで分割する
 $tag = preg_replace('/　/', ' ', $tag); //全角スペースを半角スペースへ
 $tag = preg_replace('/\s+/', ' ', $tag); //連続する半角スペースを1つの半角スペースへ
 //" "で区切る
 $tags = explode(" ", $tag);
 //tagの個数分登録を繰り返す
-foreach ($tags as $tag) {
-  //tag_idの取得
-  $sql = 'SELECT * FROM tags WHERE tag=:tag';
-  $stmt = $pdo->prepare($sql);
-  $stmt->bindValue(':tag', $tag);
-  $stmt->execute();
-  $tags_id = $stmt->fetch();
 
-  //もしtagがDBになかったら登録する、あればスルー
-  if (empty($tags_id)) {
-    $sql = "INSERT INTO `tags` (`id`, `user_id`, `tag`) VALUES (NULL, '1', :tag ) ";
+foreach ($tags as $tag) {
+    //tag_idの取得
+    $sql = 'SELECT * FROM tags WHERE tag=:tag';
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':tag', $tag);
-    $result3 = $stmt->execute();
-    $tags_id = $pdo->lastInsertId();
-  } else {
-    $tags_id = $tags_id['id'];
-  }
+    $stmt->execute();
+    $tags_id = $stmt->fetch();
+
+    //もしtagがDBになかったら登録する、あればスルー
+    if (empty($tags_id)) {
+        $sql = "INSERT INTO `tags` (`id`, `user_id`, `tag`) VALUES (NULL,  :user_id, :tag ) ";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':user_id', $userId);
+        $stmt->bindValue(':tag', $tag);
+        $stmt->execute();
+        $tags_id = $pdo->lastInsertId();
+    } else {
+        $tags_id = $tags_id['id'];
+    }
+
+<<<<<<< Updated upstream
+//タグが空の場合は登録処理が不要のためスキップする
+if (!empty($string_tag)) {
+    //tagsテーブルへのレコード追加
+    //複数のタグを分ける処理
+    $tag_id_array = array(); //arrayの初期化
+    $tag = explode(" ", $string_tag);
+    foreach ($tag as $t) {
+        $sql = "insert into tags (tag, user_id) values (:tag, :user_id)";
+        $sth = $pdo->prepare($sql);
+        $sth->bindValue(':tag', $t, PDO::PARAM_STR);
+        $sth->bindValue(':user_id', $userId, PDO::PARAM_STR);
+        $result = $sth->execute();
+        $last_tag_id = $pdo->lastInsertId();
+        array_push($tag_id_array, $last_tag_id);
+    }
 
 
+=======
+<<<<<<< HEAD
+    $sql = "INSERT INTO `apl_tag` (`id`,`apl_id`, `tag_id`) VALUES (NULL, :apl_id,:tag_id)";
+    $sth = $pdo->prepare($sql);
+    $sth->bindValue(':apl_id', $apl_id );
+    $sth->bindValue(':tag_id', $tags_id);
+    $result2 = $sth->execute();
+}
+
+
+//チェック用
+if ($result2) {
+    header('Location: ./list.php');
+} else {
+    echo '1,失敗';
+=======
 $last_dead_id = $pdo->lastInsertId();
 
 //タグが空の場合は登録処理が不要のためスキップする
@@ -71,6 +109,7 @@ if (!empty($string_tag)) {
     }
 
 
+>>>>>>> Stashed changes
     //上二つ登録したidをapl_tagテーブルへ登録する
     foreach ($tag_id_array as $t) {
         $sql = "insert into apl_tag (apl_id, tag_id) values(:apl_id, :tag_id)";
@@ -98,4 +137,8 @@ if ($result1) {
     header('Location: ./list.php');
 } else {
     echo '1失敗';
+<<<<<<< Updated upstream
+=======
+>>>>>>> b6dd4a010bc0ffa3c72f109d0a05f65c6f6755ec
+>>>>>>> Stashed changes
 }
